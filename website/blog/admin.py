6cbda2c934ad9a305331443c61c8c9601d95048e
Sergey <darkus007@yandex.ru>
2023-01-15 16:40:30 +0300

    Добавлены модели Category и Post.

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Category, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', )  # столбцы, которые выводятся в админ панели
+    list_display_links = ('title', )  # переход на правку записей по этим полям
+    search_fields = ('title', )  # последовательность имен полей, по которым должна выполняться фильтрация
+    prepopulated_fields = {'slug': ('title',)}  # заполнение поля 'slug' на основании поля 'title'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'cat')  # столбцы, которые выводятся в админ панели
+    list_display_links = ('title', )  # переход на правку записей по этим полям
+    search_fields = ('title', 'content', 'user', 'cat', 'time_created')  # последовательность имен полей, по которым должна выполняться фильтрация
+    prepopulated_fields = {'slug': ('title',)}  # заполнение поля 'slug' на основании поля 'title'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', verbose_name='Автор поста')
+    cat = models.ForeignKey(Category, on_delete=models.SET(1),
+                            related_name='category', verbose_name='Категория')  # первая кат-рия будет "Разное"
+    title = models.CharField(max_length=255, verbose_name='Наименование поста')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    body = models.TextField(verbose_name='Текст поста')
+    time_created = models.DateTimeField(auto_now=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return f'{self.title} - {self.user}'
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-time_created', 'cat']
