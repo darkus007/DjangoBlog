c0ca656dd9d7534d35e0bd756962663ed6ae5fcd
Sergey <darkus007@yandex.ru>
2023-01-23 12:12:27 +0300

    Оптимизированы шаблоны, удалено дублирование кода.

--- website/members/views.py
+++ website/members/views.py
@@ -13,14 +13,21 @@ from website.utilites import slugify
 class UserRegistrationView(CreateView):
     # form_class = UserCreationForm
     form_class = UserRegistrationForm
-    template_name = 'registration/register.html'
+    template_name = 'registration/register_or_edit_user.html'   # шаблон по умолчанию register.html
     success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Новый пользователь'
+        context['button'] = 'Регистрация'
+        context['extra_link'] = False
+        return context
 
 
 class UserChangeView(UpdateView):
     # form_class = UserChangeForm
     form_class = EditUserForm
-    template_name = 'registration/edit_user.html'
+    template_name = 'registration/register_or_edit_user.html'
     success_url = reverse_lazy('home')
 
     def get_object(self, queryset=None):
@@ -29,6 +36,13 @@ class UserChangeView(UpdateView):
         данными текущего авторизованного пользователя.
         """
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактировать пользователя'
+        context['button'] = 'Обновить профиль'
+        context['extra_link'] = True
+        return context
 
 
 class UserPasswordChangeView(PasswordChangeView):
@@ -45,7 +59,7 @@ def password_changed(request):
 
 class UserProfileView(DetailView):
     model = Profile
-    template_name = 'registration/user_profile.html'
+    template_name = 'registration/user_profile_show.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -57,7 +71,7 @@ class UserProfileView(DetailView):
 class CreateUserProfileView(CreateView):
     model = Profile
     form_class = ProfilePageForm
-    template_name = 'registration/user_profile_create.html'
+    template_name = 'registration/user_profile_create_update.html'
     # fields = '__all__'  # поля теперь прописаны в ProfilePageForm
 
     def form_valid(self, form):
@@ -66,10 +80,24 @@ class CreateUserProfileView(CreateView):
         form.instance.slug = slugify(str(self.request.user.username) + '-profile')
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Создать страницу профиля'
+        context['button'] = 'Создать страницу профиля'
+        context['extra_msg'] = 'для создания профиля.'
+        return context
+
 
 class EditUserProfileView(UpdateView):
     model = Profile
     form_class = ProfilePageForm
-    template_name = 'registration/user_profile_update.html'
+    template_name = 'registration/user_profile_create_update.html'
     # fields = ['bio', 'image', 'website_url', 'git_url', 'ya_url', 'vk_url', 'ok_url']
     success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Обновить страницу профиля'
+        context['button'] = 'Обновить страницу профиля'
+        context['extra_msg'] = 'для обновления профиля.'
+        return context
