de04fd565645bd953532b089591b45e664807fb6
Sergey <darkus007@yandex.ru>
2023-03-01 23:06:24 +0300

    Добавлена страница 404.

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler404 = 'blog.views.pageNotFound'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
